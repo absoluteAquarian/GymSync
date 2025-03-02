813e4cd908457144d48087cc00be6910e0bdacc0
mamccorkle <89654757+mamccorkle@users.noreply.github.com>
2025-03-01 21:05:50 -0600

    Commit - 202503012105 - MM
    Commit - 202503012105
    
    * Login.razor - Refactored the login page to query and verify the user. Also, implemented the SHA256 algorithm to validate the users password.
    * Register.razor - Refactored the registration page to submit the data to the database as well as hash the pasword via SHA256
    * GymSync.csproj - Added the following references:
        <PackageReference Include="Microsoft.AspNetCore.Components.Authorization" Version="8.0.2" />
        <PackageReference Include="Microsoft.AspNetCore.Components.WebAssembly.Authentication" Version="8.0.2" />
        <PackageReference Include="Microsoft.AspNetCore.Identity" Version="2.3.0" />
    * UserEntity.cs - Changed the User members to nullable

--- Models/UserEntity.cs
+++ Models/UserEntity.cs
@@ -8,10 +8,10 @@ namespace GymSync.Models
         [Key]
         public int user_id { get; set; }
         [DisplayName("First Name")]
-        public string firstName { get; set; }
+        public string? firstName { get; set; }
         [DisplayName("Last Name")]
-        public string lastName { get; set; }
-        public string userPassword { get; set; }
-        public string email { get; set; }
+        public string? lastName { get; set; }
+        public string? userPassword { get; set; }
+        public string? email { get; set; }
     }
 }
