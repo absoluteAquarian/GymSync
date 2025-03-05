79effd9a36650d0ee1dc21788fb82e1de49be424
David Wright <david.a.wright.work@gmail.com>
2025-03-04 20:38:14 -0600

    Add staff query for user information and job data.  Added table and formatted table data.

--- Views/StaffJobView.cs
+++ Views/StaffJobView.cs
@@ -2,5 +2,5 @@
 
 namespace GymSync.Views
 {
-    public record class StaffJobView(int UserID, string FirstName, string LastName, string JobName);
+    public record class StaffJobView(int UserID, string FirstName, string LastName, string JobName, string JobDescription, float HourlyWage);
 }
