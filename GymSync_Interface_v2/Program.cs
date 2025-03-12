73d379616f33344ebacdc40afcc5d851b67b6d28
mamccorkle <89654757+mamccorkle@users.noreply.github.com>
2025-03-11 21:40:37 -0500

    Added database connections and migrations

--- GymSync_Interface_v2/Program.cs
+++ GymSync_Interface_v2/Program.cs
@@ -4,6 +4,8 @@ using GymSync_Interface_v2.Data;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore.SqlServer;
 
 namespace GymSync_Interface_v2
 {
@@ -12,6 +14,9 @@ namespace GymSync_Interface_v2
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+
+
 
             // Add services to the container.
             builder.Services.AddRazorComponents()
@@ -29,9 +34,18 @@ namespace GymSync_Interface_v2
                 })
                 .AddIdentityCookies();
 
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+            var names = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+
+            var manifestStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("GymSync_Interface_v2.connection.txt")
+                ?? throw new InvalidOperationException("Could not find the connection string within the assembly's embedded resources.");
+            string connectionString;
+            using (var reader = new StreamReader(manifestStream))
+                connectionString = reader.ReadToEnd();
+
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 40));
+            //builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(connectionString, serverVersion));
+
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
@@ -41,14 +55,13 @@ namespace GymSync_Interface_v2
 
             builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
+
+
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseMigrationsEndPoint();
-            }
-            else
+            if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -63,10 +76,8 @@ namespace GymSync_Interface_v2
             app.MapRazorComponents<App>()
                 .AddInteractiveServerRenderMode();
 
-            // Add additional endpoints required by the Identity /Account Razor components.
-            app.MapAdditionalIdentityEndpoints();
-
             app.Run();
+
         }
     }
 }
