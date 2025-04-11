b6b62ea2d147d407f5bcabade26b4d049aa1e730
David Wright <david.a.wright.work@gmail.com>
2025-04-11 18:41:40 -0500

    Add dashboard redirect functionality

--- Services/UserSessionService.cs
+++ Services/UserSessionService.cs
@@ -1,5 +1,8 @@
 ﻿namespace GymSync.Services {
 	public class UserSessionService {
-		public int? UserID { get; set; } 
+		public int? UserID { get; set; }
+		public int? ClientID {get; set;}
+		public int TrainerID { get; set; }
+		public int StaffID { get; set; }
 	}
 }
