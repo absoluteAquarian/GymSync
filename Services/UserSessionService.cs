13c7afc66e51ad5a2c5121f91e8fa5343a85ded0
David Wright <david.a.wright.work@gmail.com>
2025-04-11 18:52:56 -0500

    refactored user variables and added roles to the usersession

--- Services/UserSessionService.cs
+++ Services/UserSessionService.cs
@@ -1,8 +1,17 @@
 ﻿namespace GymSync.Services {
 	public class UserSessionService {
+		//reference for user ids across the site
 		public int? UserID { get; set; }
 		public int? ClientID {get; set;}
 		public int TrainerID { get; set; }
 		public int StaffID { get; set; }
+
+		//credentials to determine what the user should have access to
+		public bool IsUser { get; set; }
+		public bool IsClient { get; set; }
+		public bool IsTrainer { get; set; }
+
+		public bool IsStaff { get; set; }
+
 	}
 }
